5b698a51bfc6afe4f76841c50c36fff4e4b664f4
Firoz Ahmed <firofame@gmail.com>
2019-11-09 03:09:11 +0530

    Updates

---
+++ precache-manifest.75c8f5709fb38d8226a2ddac3016671f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f76b5a3f3f5fbe3db23872f4ba4874b",
+    "url": "/my-form/index.html"
+  },
+  {
+    "revision": "4c81435e1f06584e24ef",
+    "url": "/my-form/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "da1a7cd2b0117a7a4bfd",
+    "url": "/my-form/static/css/main.9ee6a5f9.chunk.css"
+  },
+  {
+    "revision": "4c81435e1f06584e24ef",
+    "url": "/my-form/static/js/2.01da4bf1.chunk.js"
+  },
+  {
+    "revision": "da1a7cd2b0117a7a4bfd",
+    "url": "/my-form/static/js/main.e50562a2.chunk.js"
+  },
+  {
+    "revision": "e723793acc4f2f74d684",
+    "url": "/my-form/static/js/runtime-main.a0fa7990.js"
+  }
+]);
