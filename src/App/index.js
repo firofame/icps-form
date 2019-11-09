76f12617b0511f0140bedccbf8ee9aa575219cdb
Firoz Ahmed <firofame@gmail.com>
2019-11-09 09:29:50 +0530

    container style

--- src/App/index.js
+++ src/App/index.js
@@ -55,163 +55,173 @@ export default function App() {
   ]);
 
   return (
-    <div className="container mt-5 bg-light">
-      <TextInput
-        label={`Category No`}
-        value={categoryNo}
-        onChange={setCategoryNo}
-      />
-      <h4 className="text-center mt-3">
-        Women And Child Development Department Integrated Child Protection
-        Scheme [ICPS]
-      </h4>
-      <TextInput
-        longInput
-        label={`Application for the Post of`}
-        value={applicationFor}
-        onChange={setApplicationFor}
-      />
-      <div className="row mt-3 d-flex align-items-center">
-        <CheckBox label="SCPS" checked={isScps} onChange={setIsScps} />
-        <CheckBox label="SARA" checked={isSara} onChange={setIsSara} />
-        <SelectBox
-          label="DCPU (Specify Dist)"
-          value={dcpu}
-          onChange={setDcpu}
-        />
-        <SelectBox
-          label="JJ B/CWC (Specify Dist)"
-          value={jjb}
-          onChange={setJjb}
-        />
-        <SelectBox
-          label="CH/OH/POS (Specify Dist)"
-          value={ch}
-          onChange={setCh}
-        />
-      </div>
-      <TextInput
-        longInput
-        toUpperCase
-        label={`1. Name (Use Capital Letters Only)`}
-        value={name}
-        onChange={setName}
-      />
-      <div className="mt-4">
-        2. Male/Female/TG (Specify):
-        <select
-          value={gender}
-          onChange={e => setGender(e.target.value)}
-          className="ml-2"
-        >
-          <option value="Male">Male</option>
-          <option value="Female">Female</option>
-          <option value="TG">TG</option>
-        </select>
-      </div>
-      <div className="mt-4">
-        3. Age & Date of Birth:
-        <input
-          type="date"
-          className="mx-2"
-          value={dob}
-          onChange={e => {
-            setDob(e.target.value);
-            setAge(getAge(e.target.value));
-          }}
-        />
-        {`Age: ${age}`}
-      </div>
-      <TextInput
-        longInput
-        toUpperCase
-        label={`4. Father's/Husband's Name`}
-        value={father}
-        onChange={setFather}
-      />
-      <div className="mt-4 d-flex align-items-center">
-        5. Place of Residence (Proof to be attached):
-        <textarea
-          rows="3"
-          className="ml-2 w-50"
-          value={residence}
-          onChange={e => setResidence(e.target.value)}
-        />
-      </div>
-      <div className="d-flex align-items-center">
+    <div className="bg-light">
+      <div className="container p-3">
         <TextInput
-          type="tel"
-          label={`6. Phone / Mobile No: 1`}
-          value={phone}
-          onChange={setPhone}
+          label={`Category No`}
+          value={categoryNo}
+          onChange={setCategoryNo}
         />
-        <TextInput type="tel" label={`2`} value={mobile} onChange={setMobile} />
-      </div>
-      <TextInput
-        longInput
-        type="email"
-        label={`7. e-mail Address`}
-        value={email}
-        onChange={setEmail}
-      />
-      <div className="mt-4 d-flex align-items-center">
-        8. Permanent Address (With Pin code):
-        <textarea
-          rows="3"
-          className="ml-2 w-50"
-          value={address}
-          onChange={e => setAddress(e.target.value)}
+        <h4 className="text-center mt-3">
+          Women And Child Development Department Integrated Child Protection
+          Scheme [ICPS]
+        </h4>
+        <TextInput
+          longInput
+          label={`Application for the Post of`}
+          value={applicationFor}
+          onChange={setApplicationFor}
         />
-      </div>
-      <div className="mt-4">
-        9. Qualification (Use Additional Sheets if needed):
-        <Table
-          columns={qualificationsTableColumns(
-            qualificationsData,
-            setqualificationsData
-          )}
-          data={qualificationsData}
+        <div className="row mt-3 d-flex align-items-center">
+          <CheckBox label="SCPS" checked={isScps} onChange={setIsScps} />
+          <CheckBox label="SARA" checked={isSara} onChange={setIsSara} />
+          <SelectBox
+            label="DCPU (Specify Dist)"
+            value={dcpu}
+            onChange={setDcpu}
+          />
+          <SelectBox
+            label="JJ B/CWC (Specify Dist)"
+            value={jjb}
+            onChange={setJjb}
+          />
+          <SelectBox
+            label="CH/OH/POS (Specify Dist)"
+            value={ch}
+            onChange={setCh}
+          />
+        </div>
+        <TextInput
+          longInput
+          toUpperCase
+          label={`1. Name (Use Capital Letters Only)`}
+          value={name}
+          onChange={setName}
         />
+        <div className="mt-4">
+          2. Male/Female/TG (Specify):
+          <select
+            value={gender}
+            onChange={e => setGender(e.target.value)}
+            className="ml-2"
+          >
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
+            <option value="TG">TG</option>
+          </select>
+        </div>
+        <div className="mt-4">
+          3. Age & Date of Birth:
+          <input
+            type="date"
+            className="mx-2"
+            value={dob}
+            onChange={e => {
+              setDob(e.target.value);
+              setAge(getAge(e.target.value));
+            }}
+          />
+          {`Age: ${age}`}
+        </div>
+        <TextInput
+          longInput
+          toUpperCase
+          label={`4. Father's/Husband's Name`}
+          value={father}
+          onChange={setFather}
+        />
+        <div className="mt-4 d-flex align-items-center">
+          5. Place of Residence (Proof to be attached):
+          <textarea
+            rows="3"
+            className="ml-2 w-50"
+            value={residence}
+            onChange={e => setResidence(e.target.value)}
+          />
+        </div>
+        <div className="d-flex align-items-center">
+          <TextInput
+            type="tel"
+            label={`6. Phone / Mobile No: 1`}
+            value={phone}
+            onChange={setPhone}
+          />
+          <TextInput
+            type="tel"
+            label={`2`}
+            value={mobile}
+            onChange={setMobile}
+          />
+        </div>
+        <TextInput
+          longInput
+          type="email"
+          label={`7. e-mail Address`}
+          value={email}
+          onChange={setEmail}
+        />
+        <div className="mt-4 d-flex align-items-center">
+          8. Permanent Address (With Pin code):
+          <textarea
+            rows="3"
+            className="ml-2 w-50"
+            value={address}
+            onChange={e => setAddress(e.target.value)}
+          />
+        </div>
+        <div className="mt-4">
+          9. Qualification (Use Additional Sheets if needed):
+          <Table
+            columns={qualificationsTableColumns(
+              qualificationsData,
+              setqualificationsData
+            )}
+            data={qualificationsData}
+          />
+        </div>
+        <TextInput
+          longInput
+          label={`10. Experience`}
+          value={experience}
+          onChange={setexperience}
+        />
+        <button
+          className="w-25 m-3 p-2"
+          onClick={() => {
+            const tempData = [
+              ...tableData,
+              {
+                categoryNo,
+                applicationFor,
+                isScps,
+                isSara,
+                dcpu,
+                jjb,
+                ch,
+                name,
+                gender,
+                dob,
+                age,
+                father,
+                residence,
+                phone,
+                mobile,
+                email,
+                address,
+                experience
+              }
+            ];
+            localStorage.setItem("tableData", JSON.stringify(tempData));
+            setTableData(tempData);
+          }}
+        >
+          Save
+        </button>
       </div>
-      <TextInput
-        longInput
-        label={`10. Experience`}
-        value={experience}
-        onChange={setexperience}
-      />
-      <button
-        className="w-25 m-3 p-2"
-        onClick={() => {
-          const tempData = [
-            ...tableData,
-            {
-              categoryNo,
-              applicationFor,
-              isScps,
-              isSara,
-              dcpu,
-              jjb,
-              ch,
-              name,
-              gender,
-              dob,
-              age,
-              father,
-              residence,
-              phone,
-              mobile,
-              email,
-              address,
-              experience
-            }
-          ];
-          localStorage.setItem("tableData", JSON.stringify(tempData));
-          setTableData(tempData);
-        }}
-      >
-        Save
-      </button>
-      <Table columns={finalTableColumns} data={tableData} />
+
+      <div className="container py-5">
+        <Table columns={finalTableColumns} data={tableData} />
+      </div>
     </div>
   );
 }
